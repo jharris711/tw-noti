212195730df2c909a4ffccf91d1aba05fc180f08
Josh Harris <49429928+jharris711@users.noreply.github.com>
2023-06-05 23:33:17 -0400

    Dev screen update

--- src/components/TestTheToast/TestTheToast.tsx
+++ src/components/TestTheToast/TestTheToast.tsx
@@ -8,12 +8,43 @@ const TestTheToast = () => {
   };
 
   return (
-    <button
-      className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800'
-      onClick={handleClick}
-    >
-      Click the button
-    </button>
+    <section className='bg-gray-50 dark:bg-gray-900'>
+      <div className='flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0'>
+        <a
+          href='#'
+          className='flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white'
+        >
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            fill='none'
+            viewBox='0 0 24 24'
+            stroke-width='1.5'
+            stroke='currentColor'
+            className='w-6 h-6 m-2 text-blue-400'
+          >
+            <path
+              stroke-linecap='round'
+              stroke-linejoin='round'
+              d='M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0M3.124 7.5A8.969 8.969 0 015.292 3m13.416 0a8.969 8.969 0 012.168 4.5'
+            />
+          </svg>
+          tw-noti
+        </a>
+        <div className='w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700'>
+          <div className='p-6 space-y-4 md:space-y-6 sm:p-8'>
+            <h1 className='text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white'>
+              Test the notifications by clicking the button
+            </h1>
+            <button
+              onClick={handleClick}
+              className='w-full text-white bg-blue-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800'
+            >
+              Toast me, baby!
+            </button>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
