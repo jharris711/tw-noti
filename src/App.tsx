9ef5099296a71df235e68e3c902966ecc2f26ea4
Josh Harris <49429928+jharris711@users.noreply.github.com>
2023-06-08 03:05:20 -0400

    Consolidate theme logic

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,10 @@ import TestTheToast from './components/TestTheToast';
 const App = () => {
   return (
     <>
-      <ToastProvider containerClasses='right-12 bottom-12'>
+      <ToastProvider
+        containerClasses='right-12 bottom-12'
+        messageClasses='dark:text-white font-extrabold'
+      >
         <TestTheToast />
       </ToastProvider>
     </>
--- src/hooks/useTheme.ts
+++ src/hooks/useTheme.ts
@@ -20,42 +20,39 @@ const useTheme = ({
   const [currentTheme, setCurrentTheme] = useState<Theme>(defaultTheme);
 
   useEffect(() => {
-    if (!buttonClasses) return;
+    if (
+      !buttonClasses &&
+      !containerClasses &&
+      !iconClasses &&
+      !layoutClasses &&
+      !messageClasses
+    )
+      return;
     const temp = currentTheme;
-    temp.button.classes = temp.button.classes + ' ' + buttonClasses;
+    if (buttonClasses)
+      temp.button.classes = temp.button.classes + ' ' + buttonClasses;
+    if (containerClasses)
+      temp.container.classes = temp.container.classes + ' ' + containerClasses;
+    if (iconClasses) {
+      Object.keys(iconClasses).forEach((key) => {
+        temp.icon.classes[key].altText = iconClasses[key].altText;
+        temp.icon.classes[key].classes =
+          temp.icon.classes[key].classes + ' ' + iconClasses[key].classes;
+      });
+    }
+    if (layoutClasses)
+      temp.layout.classes = temp.layout.classes + ' ' + layoutClasses;
+    if (messageClasses)
+      temp.message.classes = temp.message.classes + ' ' + messageClasses;
     setCurrentTheme(temp);
-  }, [buttonClasses, currentTheme]);
-
-  useEffect(() => {
-    if (!containerClasses) return;
-    const temp = currentTheme;
-    temp.container.classes = temp.container.classes + ' ' + containerClasses;
-    setCurrentTheme(temp);
-  }, [containerClasses, currentTheme]);
-
-  useEffect(() => {
-    if (!iconClasses) return;
-    const temp = currentTheme;
-    Object.keys(iconClasses).forEach((key) => {
-      temp.icon.classes[key].altText = iconClasses[key].altText;
-      temp.icon.classes[key].classes =
-        temp.icon.classes[key].classes + ' ' + iconClasses[key].classes;
-    });
-  }, [iconClasses, currentTheme]);
-
-  useEffect(() => {
-    if (!layoutClasses) return;
-    const temp = currentTheme;
-    temp.layout.classes = temp.layout.classes + ' ' + layoutClasses;
-    setCurrentTheme(temp);
-  }, [layoutClasses, currentTheme]);
-
-  useEffect(() => {
-    if (!messageClasses) return;
-    const temp = currentTheme;
-    temp.message.classes = temp.message.classes + ' ' + messageClasses;
-    setCurrentTheme(temp);
-  }, [messageClasses, currentTheme]);
+  }, [
+    buttonClasses,
+    containerClasses,
+    iconClasses,
+    layoutClasses,
+    messageClasses,
+    currentTheme,
+  ]);
 
   return { currentTheme, setCurrentTheme };
 };
