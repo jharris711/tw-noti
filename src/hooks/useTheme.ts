1267f1901a01048daf176d99a11ad2876930116e
Josh Harris <49429928+jharris711@users.noreply.github.com>
2023-06-09 16:39:46 -0400

    Fix bug with top position

--- src/hooks/useTheme.ts
+++ src/hooks/useTheme.ts
@@ -2,6 +2,31 @@ import { useState, useEffect } from 'react';
 import { defaultTheme } from '../theme/defaultTheme';
 import { Theme } from '../types/Theme';
 
+function filterClasses(values: string[], classString: string): string {
+  // Create a regular expression pattern to match the opposite values
+  const oppositePattern = new RegExp(
+    `(\\b(?:${values.join('|')})-\\d+\\b)`,
+    'g'
+  );
+
+  // Remove classes that match the opposite pattern
+  const filteredClasses = classString.replace(
+    oppositePattern,
+    (match, className) => {
+      const [oppositeValue] = className.split('-');
+      return values.includes(oppositeValue) ? className : '';
+    }
+  );
+
+  // Trim and remove extra spaces between classes
+  const trimmedClasses = filteredClasses.replace(/\s+/g, ' ').trim();
+
+  return trimmedClasses;
+}
+
+const filterDuplicateValues = (inputString: string): string =>
+  [...new Set(inputString.split(' '))].join(' ');
+
 interface Props {
   buttonClasses?: Theme['button']['classes'];
   containerClasses?: Theme['container']['classes'];
@@ -31,8 +56,16 @@ const useTheme = ({
     const temp = currentTheme;
     if (buttonClasses)
       temp.button.classes = temp.button.classes + ' ' + buttonClasses;
-    if (containerClasses)
+
+    if (containerClasses) {
       temp.container.classes = temp.container.classes + ' ' + containerClasses;
+      const values = containerClasses
+        .split(' ')
+        .map((value) => value.split('-')[0] || '');
+      temp.container.classes = filterClasses(values, temp.container.classes);
+      temp.container.classes = filterDuplicateValues(temp.container.classes);
+    }
+
     if (iconClasses) {
       Object.keys(iconClasses).forEach((key) => {
         temp.icon.classes[key].altText = iconClasses[key].altText;
