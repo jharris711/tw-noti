8fe75b7fd23b04d78b4776212d3eaf449c8fd87f
Josh Harris <49429928+jharris711@users.noreply.github.com>
2024-06-09 02:43:37 -0400

    Add tests for Example component

--- src/components/Example/Example.tsx
+++ src/components/Example/Example.tsx
@@ -4,7 +4,8 @@ export function Example() {
   const { enqueueToast } = useToast();
 
   const handleClick = () => {
-    enqueueToast({ content: 'This is a notification', type: 'info' });
+    const toast = { content: 'This is a notification', type: 'info' };
+    enqueueToast(toast);
   };
 
   return (
@@ -39,9 +40,10 @@ export function Example() {
               Test the notifications by clicking the button
             </h1>
             <button
-              onClick={handleClick}
               className='w-full text-white bg-blue-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800'
               data-testid='example-component-toast-trigger-button'
+              onClick={handleClick}
+              type='button'
             >
               Toast me, baby!
             </button>
